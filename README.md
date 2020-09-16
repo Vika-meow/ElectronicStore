5dbe6f5d43a3e75970af5991fcf73f3068210d96
Vika-meow <viktoriagnezdilova@gmail.com>
2020-09-16 10:52:07 +0700

    all requests done

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ skip it now
 ##### Схема базы данных
 ![](docs/DatabaseSchema.png "Scheme")
 ##### Стартовое наполнение базы данных
-В папке [resources/init](./src/main/resources/init) лежат данные (в сsv) формате, которыми наполняется база данных. 
+В папке [resources/init](./src/main/resources/init) лежат данные (в сsv), которыми наполняется база данных. 
 ## Как этим пользоваться
--- src/main/java/com/github/vika/store/controller/ProductController.java
+++ src/main/java/com/github/vika/store/controller/ProductController.java
@@ -3,12 +3,12 @@ package com.github.vika.store.controller;
 import com.github.vika.store.domain.Product;
 import com.github.vika.store.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
 
 @RestController
 public class ProductController {
@@ -16,12 +16,14 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
+    /*Get all products*/
     @GetMapping("/product/")
     public Iterable<Product> list() {
         return productRepository.findAll();
     }
 
-    @PostMapping(value = "/product/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    /*Add new product*/
+    @PostMapping("/product/")
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
         Product savedProduct = productRepository.save(product);
 
@@ -29,5 +31,42 @@ public class ProductController {
                 ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedProduct.getId()).toUri();
 
         return ResponseEntity.created(location).body(savedProduct);
+    }
+
+    /*Get product by id*/
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> retrieveOrder(@PathVariable int id) {
+        Optional<Product> product = productRepository.findById(id);
+
+        if (!product.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product.get());
+    }
+
+    /*Change existing product*/
+    @PutMapping("/product/{id}")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product newProduct, @PathVariable int id) {
+        Optional<Product> stored = productRepository.findById(id);
+
+        if (!stored.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Product updated = stored.get();
+        updated.setSerialNumber(newProduct.getSerialNumber());
+        updated.setProductType(newProduct.getProductType());
+        updated.setInfo(newProduct.getInfo());
+        updated.setManufacturer(newProduct.getManufacturer());
+        updated.setPrice(newProduct.getPrice());
+        updated.setCount(newProduct.getCount());
+
+        return ResponseEntity.ok(updated);
+    }
+
+    /*Get products with one type*/
+    @GetMapping("/product/byType")
+    public Iterable<Product> getProductsByType(@RequestParam int productType){
+        return productRepository.getByProductType_Id(productType);
     }
 }
